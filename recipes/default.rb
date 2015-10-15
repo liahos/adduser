b09a146e9fac746eefbd6e4360eac0dc5fd3e09d
liahos <soohailshaikh@gmail.com>
2015-10-15 15:29:38 +0530

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: adduser
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+user "liahos" do
+  home '/home/liahos'
+end
